4ff27a04b097f1dd6c5c646194e9290ff6cf5f4b
cz <cyz36@pm.me>
2024-09-12 15:37:27 +0800

    show the current focused title only

--- applet.js
+++ applet.js
@@ -1,6 +1,5 @@
 const Cinnamon = imports.gi.Cinnamon;
 const Clutter = imports.gi.Clutter;
-const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const Meta = imports.gi.Meta;
@@ -13,52 +12,17 @@ const SignalManager = imports.misc.signalManager;
 
 const MAX_TEXT_LENGTH = 1000;
 
-class AppMenuButton {
+class PPSub {
 	constructor(applet, metaWindow, transient) {
-		this.actor = new Cinnamon.GenericContainer({
-			name: 'appMenu',
-			style_class: 'window-list-item-box',
-			reactive: true,
-			can_focus: true,
-			track_hover: true });
-
 		this._applet = applet;
+		this._windows = this._applet._windows;
 		this.metaWindow = metaWindow;
 		this.transient = transient;
-
-		this.drawLabel = false;
-		this.labelVisiblePref = false;
-		this._signals = new SignalManager.SignalManager();
-		this.xid = metaWindow.get_xwindow();
-
-		if (this._applet.orientation == St.Side.TOP)
-			this.actor.add_style_class_name('top');
-		else if (this._applet.orientation == St.Side.BOTTOM)
-			this.actor.add_style_class_name('bottom');
-		else if (this._applet.orientation == St.Side.LEFT)
-			this.actor.add_style_class_name('left');
-		else if (this._applet.orientation == St.Side.RIGHT)
-			this.actor.add_style_class_name('right');
-
-		this.actor._delegate = this;
-
-		this._signals.connect(this.actor, 'get-preferred-width', Lang.bind(this, this._getPreferredWidth));
-		this._signals.connect(this.actor, 'get-preferred-height', Lang.bind(this, this._getPreferredHeight));
-		this._signals.connect(this.actor, 'allocate', Lang.bind(this, this._allocate));
-
-		this._iconBox = new Cinnamon.Slicer({ name: 'appMenuIcon' });
-		this.actor.add_actor(this._iconBox);
-
-		this._label = new St.Label();
-		this.actor.add_actor(this._label);
-
-		this.updateLabelVisible();
-
-		this._windows = this._applet._windows;
 
 		this.setDisplayTitle();
 		this.onFocus();
 
+		this._signals = new SignalManager.SignalManager();
 		this._signals.connect(this.metaWindow, 'notify::title', this.setDisplayTitle, this);
 		this._signals.connect(this.metaWindow, "notify::minimized", this.setDisplayTitle, this);
 		this._signals.connect(this.metaWindow, "notify::tile-mode", this.setDisplayTitle, this);
@@ -82,12 +46,11 @@ class AppMenuButton {
 		if (title.length > MAX_TEXT_LENGTH)
 			title = title.substr(0, MAX_TEXT_LENGTH);
 
-		this._label.set_text(title);
+		this._applet.set_applet_label(title);
 	}
 
 	destroy() {
 		this._signals.disconnectAllSignals();
-		this.actor.destroy();
 	}
 
 	_hasFocus() {
@@ -106,148 +69,23 @@ class AppMenuButton {
 	onFocus() {
 		if(this._hasFocus()){
 			this.setDisplayTitle();
-		} else {
-			this._label.set_text("[ ]");
 		}
-	}
-
-	_getPreferredWidth(actor, forHeight, alloc) {
-		let [minSize, naturalSize] = this._iconBox.get_preferred_width(forHeight);
-
-		alloc.min_size = 1 * global.ui_scale;
-
-		if (this._applet.orientation == St.Side.TOP || this._applet.orientation == St.Side.BOTTOM ) {
-			// the 'buttons use entire space' option only makes sense on horizontal panels with labels
-			if (this.labelVisiblePref) {
-				alloc.natural_size = this._applet.buttonWidth * global.ui_scale;
-			} else {
-				alloc.natural_size = naturalSize
-			}
-		} else {
-			alloc.natural_size = this._applet._panelHeight;
-		}
-	}
-
-	_getPreferredHeight(actor, forWidth, alloc) {
-		let [minSize1, naturalSize1] = this._iconBox.get_preferred_height(forWidth);
-
-		if (this.labelVisiblePref) {
-			let [minSize2, naturalSize2] = this._label.get_preferred_height(forWidth);
-			alloc.min_size = Math.max(minSize1, minSize2);
-		} else {
-			alloc.min_size = minSize1;
-		}
-
-		if (this._applet.orientation == St.Side.TOP || this._applet.orientation == St.Side.BOTTOM ) {
-			/* putting a container around the actor for layout management reasons affects the allocation,
-							 causing the visible border to pull in close around the contents which is not the desired
-							 (pre-existing) behaviour, so need to push the visible border back towards the panel edge.
-							 Assigning the natural size to the full panel height used to cause recursion errors but seems fine now.
-							 If this happens to avoid this you can subtract 1 or 2 pixels, but this will give an unreactive
-							 strip at the edge of the screen */
-			alloc.natural_size = this._applet._panelHeight;
-		} else {
-			alloc.natural_size = naturalSize1;
-		}
-	}
-
-	_allocate(actor, box, flags) {
-		let allocWidth = box.x2 - box.x1;
-		let allocHeight = box.y2 - box.y1;
-
-		let childBox = new Clutter.ActorBox();
-
-		let [minWidth, minHeight, naturalWidth, naturalHeight] = this._iconBox.get_preferred_size();
-
-		let direction = this.actor.get_text_direction();
-		let spacing = Math.floor(this.actor.get_theme_node().get_length('spacing'));
-		let yPadding = Math.floor(Math.max(0, allocHeight - naturalHeight) / 2);
-
-		childBox.y1 = box.y1 + yPadding;
-		childBox.y2 = childBox.y1 + Math.min(naturalHeight, allocHeight);
-
-		if (allocWidth < naturalWidth) {
-			this.labelVisible = false;
-		} else {
-			this.labelVisible = this.labelVisiblePref;
-		}
-
-		if (this.drawLabel) {
-			if (direction === Clutter.TextDirection.LTR) {
-				childBox.x1 = box.x1;
-			} else {
-				childBox.x1 = Math.max(box.x1, box.x2 - naturalWidth);
-			}
-			childBox.x2 = Math.min(childBox.x1 + naturalWidth, box.x2);
-		} else {
-			childBox.x1 = box.x1 + Math.floor(Math.max(0, allocWidth - naturalWidth) / 2);
-			childBox.x2 = Math.min(childBox.x1 + naturalWidth, box.x2);
-		}
-		this._iconBox.allocate(childBox, flags);
-
-		if (this.drawLabel) {
-			[minWidth, minHeight, naturalWidth, naturalHeight] = this._label.get_preferred_size();
-
-			yPadding = Math.floor(Math.max(0, allocHeight - naturalHeight) / 2);
-			childBox.y1 = box.y1 + yPadding;
-			childBox.y2 = childBox.y1 + Math.min(naturalHeight, allocHeight);
-			if (direction === Clutter.TextDirection.LTR) {
-				// Reuse the values from the previous allocation
-				childBox.x1 = Math.min(childBox.x2 + spacing, box.x2);
-				childBox.x2 = box.x2;
-			} else {
-				childBox.x2 = Math.max(childBox.x1 - spacing, box.x1);
-				childBox.x1 = box.x1;
-			}
-
-			this._label.allocate(childBox, flags);
-		}
-
-		childBox.x1 = 0;
-		childBox.y1 = 0;
-		childBox.x2 = this.actor.width;
-		childBox.y2 = this.actor.height;
-	}
-
-	updateLabelVisible() {
-		this._label.show();
-		this.labelVisiblePref = true;
-		this.drawLabel = true;
 	}
 };
 
-class PPlet extends Applet.Applet {
-	constructor(orientation, panel_height, instance_id) {
-		super(orientation, panel_height, instance_id);
+function PPLet(orientation, panel_height, instance_id) {
+	this._init(orientation, panel_height, instance_id);
+}
 
-		this.signals = new SignalManager.SignalManager(null);
+PPLet.prototype = {
+	__proto__: Applet.TextApplet.prototype,
 
-		this.setAllowedLayout(Applet.AllowedLayout.BOTH);
+	_init: function(orientation, panel_height, instance_id) {
+		Applet.TextApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
 		this.actor.set_track_hover(false);
-		this.actor.set_style_class_name("window-list-box");
-		this.orientation = orientation;
 		this.appletEnabled = false;
-		//
-		// A layout manager is used to cater for vertical panels as well as horizontal
-		//
-		let manager;
-		if (this.orientation == St.Side.TOP || this.orientation == St.Side.BOTTOM) {
-			manager = new Clutter.BoxLayout( { orientation: Clutter.Orientation.HORIZONTAL });
-		} else {
-			manager = new Clutter.BoxLayout( { orientation: Clutter.Orientation.VERTICAL });
-			this.actor.add_style_class_name("vertical");
-		}
 
-		this.manager = manager;
-		this.manager_container = new Clutter.Actor( { layout_manager: manager } );
-		this.actor.add_actor (this.manager_container);
-
-		this.dragInProgress = false;
-		this._tooltipShowing = false;
-		this._tooltipErodeTimer = null;
-		this._menuOpen = false;
-		this._urgentSignal = null;
 		this._windows = [];
 		this._monitorWatchList = [];
 
@@ -256,6 +94,7 @@ class PPlet extends Applet.Applet {
 		this._updateLabels();
 		this.showAllWorkspaces = true;
 
+		this.signals = new SignalManager.SignalManager(null);
 		this.signals.connect(global.display, 'window-created', this._onWindowAddedAsync, this);
 		this.signals.connect(global.display, 'window-monitor-changed', this._onWindowMonitorChanged, this);
 		this.signals.connect(global.display, 'window-workspace-changed', this._onWindowWorkspaceChanged, this);
@@ -263,97 +102,39 @@ class PPlet extends Applet.Applet {
 		this.signals.connect(Main.panelManager, 'monitors-changed', this._updateWatchedMonitors, this);
 		this.signals.connect(global.window_manager, 'switch-workspace', this._refreshAllItems, this);
 		this.signals.connect(Cinnamon.WindowTracker.get_default(), "window-app-changed", this._onWindowAppChanged, this);
-
-		this.signals.connect(this.actor, 'style-changed', Lang.bind(this, this._updateSpacing));
-
-		this.on_orientation_changed(orientation);
-	}
+	},
 
 	on_applet_added_to_panel(userEnabled) {
-		this._updateSpacing();
 		this.appletEnabled = true;
-	}
+	},
 
 	on_applet_removed_from_panel() {
 		this.signals.disconnectAllSignals();
-	}
+		for (let window of windows) {
+			window.destroy();
+		}
+	},
 
 	on_applet_instances_changed() {
 		this._updateWatchedMonitors();
-	}
+	},
 
 	on_panel_height_changed() {
 		this._refreshAllItems();
-	}
+	},
 
 	on_panel_icon_size_changed(size) {
 		this._refreshAllItems();
-	}
-
-	on_orientation_changed(orientation) {
-		this.orientation = orientation;
-
-		for (let window of this._windows)
-			window.updateLabelVisible();
-
-		if (orientation == St.Side.TOP || orientation == St.Side.BOTTOM) {
-			this.manager.set_vertical(false);
-			this._reTitleItems();
-			this.actor.remove_style_class_name("vertical");
-		} else {
-			this.manager.set_vertical(true);
-			this.actor.add_style_class_name("vertical");
-			this.actor.set_x_align(Clutter.ActorAlign.CENTER);
-			this.actor.set_important(true);
-		}
-
-		if (orientation == St.Side.TOP) {
-			for (let child of this.manager_container.get_children()) {
-				child.set_style_class_name('window-list-item-box top');
-				child.set_style('margin-top: 0px; padding-top: 0px;');
-			}
-			this.actor.set_style('margin-top: 0px; padding-top: 0px;');
-		} else if (orientation == St.Side.BOTTOM) {
-			for (let child of this.manager_container.get_children()) {
-				child.set_style_class_name('window-list-item-box bottom');
-				child.set_style('margin-bottom: 0px; padding-bottom: 0px;');
-			}
-			this.actor.set_style('margin-bottom: 0px; padding-bottom: 0px;');
-		} else if (orientation == St.Side.LEFT) {
-			for (let child of this.manager_container.get_children()) {
-				child.set_style_class_name('window-list-item-box left');
-				child.set_style('margin-left 0px; padding-left: 0px; padding-right: 0px; margin-right: 0px;');
-				child.set_x_align(Clutter.ActorAlign.CENTER);
-			}
-			this.actor.set_style('margin-left: 0px; padding-left: 0px; padding-right: 0px; margin-right: 0px;');
-		} else if (orientation == St.Side.RIGHT) {
-			for (let child of this.manager_container.get_children()) {
-				child.set_style_class_name('window-list-item-box right');
-				child.set_style('margin-left: 0px; padding-left: 0px; padding-right: 0px; margin-right: 0px;');
-				child.set_x_align(Clutter.ActorAlign.CENTER);
-			}
-			this.actor.set_style('margin-right: 0px; padding-right: 0px; padding-left: 0px; margin-left: 0px;');
-		}
-
-		if (this.appletEnabled) {
-			this._updateSpacing();
-		}
-	}
-
-	_updateSpacing() {
-		let themeNode = this.actor.get_theme_node();
-		let spacing = themeNode.get_length('spacing');
-		this.manager.set_spacing(spacing * global.ui_scale);
-	}
+	},
 
 	_onWindowAddedAsync(display, metaWindow, monitor) {
 		Mainloop.timeout_add(20, Lang.bind(this, this._onWindowAdded, display, metaWindow, monitor));
-	}
+	},
 
 	_onWindowAdded(display, metaWindow, monitor) {
 		if (this._shouldAdd(metaWindow))
 			this._addWindow(metaWindow, false);
-	}
+	},
 
 	_onWindowMonitorChanged(display, metaWindow, monitor) {
 		if (this._shouldAdd(metaWindow))
@@ -363,22 +144,22 @@ class PPlet extends Applet.Applet {
 			this._removeWindow(metaWindow);
 			this.refreshing = false;
 		}
-	}
+	},
 
 	_refreshItemByMetaWindow(metaWindow) {
 		let window = this._windows.find(win => (win.metaWindow == metaWindow));
 
 		if (window)
 			this._refreshItem(window);
-	}
+	},
 
 	_onWindowWorkspaceChanged(display, metaWindow, metaWorkspace) {
 		this._refreshItemByMetaWindow(metaWindow);
-	}
+	},
 
 	_onWindowAppChanged(tracker, metaWindow) {
 		this._refreshItemByMetaWindow(metaWindow);
-	}
+	},
 
 	_onWindowSkipTaskbarChanged(display, metaWindow) {
 		if (metaWindow && metaWindow.is_skip_taskbar()) {
@@ -387,7 +168,7 @@ class PPlet extends Applet.Applet {
 		}
 
 		this._onWindowAdded(display, metaWindow, 0);
-	}
+	},
 
 	_refreshItem(window) {
 		window.actor.visible =
@@ -401,24 +182,24 @@ class PPlet extends Applet.Applet {
 		 * one isn't shown! */
 		if (window.transient)
 			window.actor.visible = !window.actor.visible;
-	}
+	},
 
 	_refreshAllItems() {
 		for (let window of this._windows) {
 			this._refreshItem(window);
 		}
-	}
+	},
 
 	_reTitleItems() {
 		for (let window of this._windows) {
 			window.setDisplayTitle();
 		}
-	}
+	},
 
 	_updateLabels() {
 		for (let window of this._windows)
 			window.updateLabelVisible();
-	}
+	},
 
 	_updateWatchedMonitors() {
 		let n_mons = global.display.get_n_monitors();
@@ -469,7 +250,7 @@ class PPlet extends Applet.Applet {
 		}
 
 		this.refreshing = false;
-	}
+	},
 
 	_addWindow(metaWindow, transient) {
 		for (let window of this._windows)
@@ -477,37 +258,20 @@ class PPlet extends Applet.Applet {
 				window.transient == transient)
 				return;
 
-		let appButton = new AppMenuButton(this, metaWindow, transient);
-		this.manager_container.add_actor(appButton.actor);
+		let appButton = new PPSub(this, metaWindow, transient);
 
 		this._windows.push(appButton);
-
-		/* We want to make the AppMenuButtons look like they are ordered by
-		 * workspace. So if we add an AppMenuButton for a window in another
-		 * workspace, put it in the right position. It is at the end by
-		 * default, so move it to the start if needed */
-		if (transient) {
-			if (metaWindow.get_workspace().index() < global.workspace_manager.get_active_workspace_index())
-				this.manager_container.set_child_at_index(appButton.actor, 0);
-		} else {
-			if (metaWindow.get_workspace() != global.workspace_manager.get_active_workspace()) {
-				if (!(this.showAllWorkspaces)) {
-					appButton.actor.hide();
-				}
-			}
-		}
-	}
+	},
 
 	_removeWindow(metaWindow) {
 		let i = this._windows.length;
-		// Do an inverse loop because we might remove some elements
 		while (i--) {
 			if (this._windows[i].metaWindow == metaWindow) {
 				this._windows[i].destroy();
 				this._windows.splice(i, 1);
 			}
 		}
-	}
+	},
 
 	_shouldAdd(metaWindow) {
 		return Main.isInteresting(metaWindow) &&
@@ -517,5 +281,5 @@ class PPlet extends Applet.Applet {
 }
 
 function main(metadata, orientation, panel_height, instance_id) {
-	return new PPlet(orientation, panel_height, instance_id);
+	return new PPLet(orientation, panel_height, instance_id);
 }
